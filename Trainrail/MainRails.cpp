d9155675481b4c0a16cf054b9d4e9a6ca29c606d
Vadym Martyniuk <xxxngbxxx@gmail.com>
2018-10-12 10:49:29 +0300

    Apriorit task with rails

---
+++ Trainrail/MainRails.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "rails.h"
+
+int main(){
+
+   Rails rail(1,7,3);
+   rail.pushRails(1,1,2);
+   rail.pushRails(1,2,4);
+   rail.pushRails(1,3,5);
+   rail.searchFree();
+   rail.print();
+   rail.~Rails();
+
+   return 0;
+}
