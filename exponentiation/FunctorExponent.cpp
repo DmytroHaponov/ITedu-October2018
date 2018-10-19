965e668c2371d5cb6980cce587f8bfa703d60da9
Vadym Martyniuk <xxxngbxxx@gmail.com>
2018-10-19 11:43:08 +0300

    Changes conditions in Functor exponentiation

--- exponentiation/FunctorExponent.cpp
+++ exponentiation/FunctorExponent.cpp
@@ -4,20 +4,21 @@ FunctorExponent::FunctorExponent(int base, int power)
     :m_base(base),
       m_power(power){}
 int::FunctorExponent::operator()(){
-    if(m_base == 0){
+
+    if(m_base == 0 && m_power >= 0){
         return 0;
     }
     else if(m_base < 0){
-	return 0;
+        return 0;
     }
     else if(m_power <= 0 && m_base <= 0){
-	return 0;
+        return 0;
     }
     else if(m_base > 0 && m_power < 0){
         return 0;
     }
     else if(m_power == 0 && m_base > 0){
-	return 1;
+        return 1;
     }
     else{
         for (int i = 1; i < m_power; i++) {
