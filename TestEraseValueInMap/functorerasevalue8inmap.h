7bdc5c8d7d2abdc09245398c56f4ebdf8181354c
Vadym Martyniuk <xxxngbxxx@gmail.com>
2018-10-19 11:50:33 +0300

    Gtest and functor which erase 8 in std::map

---
+++ TestEraseValueInMap/functorerasevalue8inmap.h
@@ -0,0 +1,13 @@
+#ifndef FUNCTORERASEVALUE8INMAP_H
+#define FUNCTORERASEVALUE8INMAP_H
+#include <map>
+
+class H8{
+ public:
+  H8();
+  std::map<int,int> operator()(const std::map<int,int> &init);
+};
+
+
+
+#endif // FUNCTORERASEVALUE8INMAP_H
