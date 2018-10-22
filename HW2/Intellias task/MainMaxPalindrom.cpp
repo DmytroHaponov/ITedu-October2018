f9cb1054d4dab4a4b36a88af7d3ba84a34964eff
Vadym Martyniuk <xxxngbxxx@gmail.com>
2018-10-22 16:25:27 +0300

    HW2 Intelias task max palindrom mult of 3-digit num

---
+++ HW2/Intellias task/MainMaxPalindrom.cpp
@@ -0,0 +1,29 @@
+#include "MaxPalindrom.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main(){
+
+    MaxPalindrom pal;
+    pal.Print();
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
