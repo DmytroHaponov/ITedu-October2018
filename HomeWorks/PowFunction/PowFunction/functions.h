c119f9da8a069177a3012e50125a1b4c0622a5b9
Your Name <you@example.com>
2019-03-18 16:13:33 +0200

    First attempt to send homeword

---
+++ HomeWorks/PowFunction/PowFunction/functions.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+double my_pow(int base, int exponent);
+
+#endif // FUNCTIONS_H
---
+++ HomeWorks/PowFunction/PowFunction/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+#include "functions.h"
+
+int main()
+{
+    std::cout << my_pow(3, 3) << std::endl;
+
+    return 0;
+}
